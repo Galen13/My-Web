aa7f9224553ff110dd2a6d9b998d75672cd2e955
Galen13 <161545840+Galen13@users.noreply.github.com>
2024-06-10 12:44:48 +0700

    Delete UAS Desain Web directory

--- UAS Desain Web/ModeGelap.js
+++
@@ -1,7 +0,0 @@
-const darkModeToggle = document.getElementById('Mode-Gelap');
-const body = document.body;
-
-darkModeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    darkModeToggle.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
-});
